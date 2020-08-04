819968657a4a461031f7df979cbf35db7e9678a9
RachelUberti <67136293+RachelUberti@users.noreply.github.com>
2020-08-04 21:41:39 +1000

    Add task form validaton

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,29 @@
-// Start Calendar appear disappear //
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+// Add Task - Add Task Name & Task Description - Validaton //
+let btnAddTaskSave = document.querySelector(".btnAddTaskSave");
+let taskName = document.querySelector(".taskName");
+let TaskNameErrMsg = document.querySelector("#TaskNameErrMsg");
+let taskDescription = document.querySelector(".taskDescription");
+let taskDescriptionErrMsg = document.querySelector("#taskDescriptionErrMsg");
+btnAddTaskSave.onclick = function () {
+  if (
+    taskName.value == "" ||
+    taskName.value.length <= 8 ||
+    taskDescription.value == ""
+  ) {
+    TaskNameErrMsg.innerHTML =
+      "Please enter a Task Name longer than 8 characters";
+    TaskNameErrMsg.style.color = "#ff0000";
+    taskName.style.borderColor = "#ff0000";
+    taskDescriptionErrMsg.innerHTML = "Please enter a Task Description";
+    taskDescriptionErrMsg.style.color = "#ff0000";
+    taskDescription.style.borderColor = "#ff0000";
+    return false;
   } else {
-    x.style.display = "none";
+    taskName.style.borderColor = "#66CDAA";
+    taskDescription.style.borderColor = "#66CDAA";
+    return true;
   }
-}
+};
+// END Add Task - Add Task Name - Validaton //
 
-// End Calendar appear disappear //
+//length validation not working
